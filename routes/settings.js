62c562717d4d9bc9c507fc16e161841e6ae8094b
Jane Dinh <janelamdinh@gmail.com>
2021-02-23 22:41:52 -0800

    rendered json for settings page

--- routes/settings.js
+++ routes/settings.js
@@ -1,4 +1,6 @@
+var settingsData = require('../settingsData.json');
+
 exports.view = function(req, res){
-  res.render('settings.handlebars');
+  res.render('settings.handlebars', settingsData);
 };
 
