d9f20548e3ec8c7fb66b355f15a649b436455fc9
tjuacalla123 <>
2021-02-26 00:21:54 -0800

    working search bar for log page

--- public/js/logpage_functions.js
+++ public/js/logpage_functions.js
@@ -1,21 +1,33 @@
 $(document).ready(function(){
   console.log("ready");
   
-  $("#create-plastic").click(function(){
-
+  $(".search-bar").keydown(function(){
+    var namePlastic;
+    var namePlasticSpaces;
+    var filter = $(this).val().toUpperCase();
+    $(".single-plastic").each(function(index) {
+      namePlastic = this.id;
+      namePlasticSpaces = namePlastic.replace(/-/g, ' ');
+      console.log(namePlasticSpaces);
+      if (namePlasticSpaces.toUpperCase().indexOf(filter) > -1) {
+        $(this).css("display", "");
+      } else {
+        $(this).css("display", "none");
+      }
+    });
   })
   
+  
+  // add all the plasticTypes that the user created 
+  var nameofPlastic;
   var plasticTypes = JSON.parse(sessionStorage.getItem("plastic-types"));
   if (plasticTypes != null) {
     for (var i = 0; i < Object.keys(plasticTypes).length; i++) {
-      console.log(i);
-      console.log("wtf");
-      $(".plastic-contents").append("<div class='single-plastic ui-draggable ui-draggable-handle' id= " + 
-      plasticTypes[i]["name"] + ">" + plasticTypes[i]["icon"] +"</div>");
+      nameofPlastic = plasticTypes[i]["name"].replace(/\s/g , "-");
+      $(".plastic-contents").append("<div class='single-plastic ui-draggable ui-draggable-handle' id=" + 
+        nameofPlastic + ">" + plasticTypes[i]["icon"] +"</div>");
     }
   }
   console.log(plasticTypes);
-  
-
   
 });
